b61d62008a2c6f2aef0a958580419c3f28cec736
MaxxCypress <74368008+MaxxCypress@users.noreply.github.com>
2021-03-11 12:32:26 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "ping",
     description: "this is the ping command!",
     execute(message, args) {
-        message.channel.send("Pong" `\n I am now ${Date.now() - message.createdTimestamp}ms late.`);
+        message.channel.send("Pong" + `\n I am now ${Date.now() - message.createdTimestamp}ms late.`);
     }
 }
